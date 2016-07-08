37c06c44960703a6fb240704090136b976b34887
andrea_cajamarca <andreac0893@tywls.org>
2016-07-08 18:18:56 +0000

    hi

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // JavaScript File
+$(document).ready(function(){
+$("#clear").click(function() {
+                        $("input").val('');
+                    });
+    
+    
+    $("#sub").click(function() {
+        var venue = $("#location").val();
+        var food = $("#food").val();
+             $.getJSON("https://api.foursquare.com/v2/venues/search?near=" + venue + "&query=" + food + "&client_id=NGTFDCMT34TUOBJF0TDZNOVUUZLI0QOS0Q1DIFZYLIBZMUQN&client_secret=XJLFBJ1WWXSHEMALWKJUQ1I4XTFCXSXIULM014QRVHN5GUJT&v=20160319", function(data) {
+
+                    var items = [];
+                    $.each(data.response.venues, function(i, val) {
+
+                        items.push("<li id='" + i + "'>" + data.response.venues[i].name + "</li>");
+                    });
+                    $("#sub").click(function() {
+                        $("<ul/>", {
+                            "class": "my-new-list",
+                            html: items.join("")
+                        }).appendTo("#result");
+                    });
+                    
+                });
+                });
+                    });
